0ed5a6763a9dc7171ca133a948e367756e800c47
0xJotave <joaovmb15@gmail.com>
2025-03-27 11:34:35 -0300

    Criando UserMapper

---
+++ pwnned/src/main/java/com/pwnned/adapter/input/mapper/UserMapper.java
@@ -0,0 +1,32 @@
+package com.pwnned.adapter.input.mapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pwnned.adapter.input.dto.UserDTO;
+import com.pwnned.adapter.output.jpa.repository.entity.UserEntity;
+import com.pwnned.domain.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User toModel(UserDTO userDTO);
+    User toModel(UserEntity userEntity);
+
+    UserDTO toDTO(User user);
+    UserEntity toEntity(User user);
+
+
+    default User toDomainFromMap(Map<String, Object> map) {
+        try {
+            return new ObjectMapper().convertValue(map, User.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error deserializing Order from map", e);
+        }
+    }
+}
