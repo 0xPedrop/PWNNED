9875a7caf0ab4247528cc49e3e5233938be5e780
0xJotave <joaovmb15@gmail.com>
2025-03-28 00:53:06 -0300

    Limpando UserDTO

--- pwnned/src/main/java/com/pwnned/adapter/input/dto/UserDTO.java
+++ pwnned/src/main/java/com/pwnned/adapter/input/dto/UserDTO.java
@@ -4,6 +4,6 @@ import java.util.UUID;
 
 public record UserDTO(UUID userId, String email, String password, String username) {
     public UserDTO() {
-        this(null, null, null, null); // Inicializa os campos com valores padrão
+        this(null, null, null, null);
     }
 }
