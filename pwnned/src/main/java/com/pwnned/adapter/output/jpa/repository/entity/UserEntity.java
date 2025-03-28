adfa0ec5123d11fd7db773c8241aa23ebfda8a1e
0xJotave <joaovmb15@gmail.com>
2025-03-28 00:52:19 -0300

    Consertando UserEntity

--- pwnned/src/main/java/com/pwnned/adapter/output/jpa/repository/entity/UserEntity.java
+++ pwnned/src/main/java/com/pwnned/adapter/output/jpa/repository/entity/UserEntity.java
@@ -2,15 +2,19 @@ package com.pwnned.adapter.output.jpa.repository.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 import java.util.UUID;
 
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class UserEntity {
     @Id
@@ -23,46 +27,4 @@ public class UserEntity {
     private String password;
 
     private String username;
-
-    public UserEntity(UUID userId, String email, String password, String username) {
-        this.userId = userId;
-        this.email = email;
-        this.password = password;
-        this.username = username;
-    }
-
-    public UserEntity() {
-    }
-
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public void setUserId(UUID userId) {
-        this.userId = userId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
