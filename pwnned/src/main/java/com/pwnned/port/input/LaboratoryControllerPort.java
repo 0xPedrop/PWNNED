0a3a6996b0e8b5d81b9a77aec01213a084519f83
0xJotave <joaovmb15@gmail.com>
2025-03-29 02:32:22 -0300

    Criando Entity, Controller, Service, Adapter e Ports de Laboratory

---
+++ pwnned/src/main/java/com/pwnned/port/input/LaboratoryControllerPort.java
@@ -0,0 +1,21 @@
+package com.pwnned.port.input;
+
+import com.pwnned.adapter.input.dto.LaboratoryDTO;
+import com.pwnned.adapter.input.dto.UserDTO;
+import com.pwnned.domain.enums.LabType;
+import com.pwnned.domain.enums.UserType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface LaboratoryControllerPort {
+    ResponseEntity<LaboratoryDTO> createLab(@RequestBody LaboratoryDTO labDTO);
+    ResponseEntity<List<LaboratoryDTO>> getAllLabs();
+    ResponseEntity<LaboratoryDTO> getSingleLab(@PathVariable UUID labId);
+    ResponseEntity<String> deleteLab(@PathVariable UUID labId);
+    ResponseEntity<String> deleteAllLabs();
+    ResponseEntity<List<LaboratoryDTO>> getLabsByType(@PathVariable LabType labType);
+}
