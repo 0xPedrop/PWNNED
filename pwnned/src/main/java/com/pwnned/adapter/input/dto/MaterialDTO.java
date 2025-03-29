8b850aebef287fed579bb4ccc4f0453ec538e043
0xJotave <joaovmb15@gmail.com>
2025-03-29 02:29:51 -0300

    Removendo optional true

--- pwnned/src/main/java/com/pwnned/adapter/input/dto/MaterialDTO.java
+++
@@ -1,9 +0,0 @@
-package com.pwnned.adapter.input.dto;
-
-import com.pwnned.domain.enums.MaterialType;
-
-public record MaterialDTO(Long materialId, String title, String author, String category, MaterialType type) {
-    public MaterialDTO() {
-        this(null, null, null, null, null);
-    }
-}
--- pwnned/src/main/java/com/pwnned/domain/enums/MaterialType.java
+++
@@ -1,6 +0,0 @@
-package com.pwnned.domain.enums;
-
-public enum MaterialType {
-    BOOK,
-    VIDEO_LESSON
-}
