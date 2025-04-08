95f8a897033c5c078c6a059e83924a89fe70083f
0xJotave <joaovmb15@gmail.com>
2025-04-07 22:34:13 -0300

    Configurando Profile-Page

--- next-pwnned/src/app/profile/page.jsx
+++ next-pwnned/src/app/profile/page.jsx
@@ -1,13 +1,12 @@
-import styles from './profile.module.css';
-import ProfileCard from '../components/profileComponents/profileCard/ProfileCard';
-import OptionsCard from '../components/profileComponents/optionsCard/OptionsCard';
+import styles from "./profile.module.css";
+import OptionsCard from "../components/profileComponents/optionsCard/OptionsCard";
 
 export default function ProfilePage() {
-    return (
-        <div className={styles.background}>
-            <div className={styles.cards}>
-                <OptionsCard />
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.background}>
+      <div className={styles.cards}>
+        <OptionsCard />
+      </div>
+    </div>
+  );
 }
