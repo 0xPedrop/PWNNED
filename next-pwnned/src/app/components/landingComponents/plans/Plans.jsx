f56da9dee5b1bc9aaa264b9c626519a5c64ef802
0xPedrop <paulo.pedro@academico.ifpb.edu.br>
2025-04-07 22:11:49 -0300

    Corrigindo Planos

--- next-pwnned/src/app/components/landingComponents/plans/Plans.jsx
+++ next-pwnned/src/app/components/landingComponents/plans/Plans.jsx
@@ -14,8 +14,9 @@ const Plans = () => {
           </div>
           <div className={styles.pricing_card_price}>R$0,00</div>
           <ul>
-            <li>Access to free content and resources only</li>
-            <li>Email support with no estimated response time.</li>
+            <li>❌ Access to all content and resources</li>
+            <li>❌ Email support with a 48-hour response time.</li>
+            <li>❌ Monthly newsletter with updates and tips.</li>
           </ul>
           <a className={styles.enroll} href="#">
             Choose Free
@@ -29,9 +30,9 @@ const Plans = () => {
           </div>
           <div className={styles.pricing_card_price}>R$12,99</div>
           <ul>
-            <li>Access to all content and resources</li>
-            <li>Email support with a 48-hour response time.</li>
-            <li>Monthly newsletter with updates and tips.</li>
+            <li>✅ Access to all content and resources</li>
+            <li>✅ Email support with a 48-hour response time.</li>
+            <li>✅ Monthly newsletter with updates and tips.</li>
           </ul>
           <a className={styles.enroll} href="#">
             Choose Premium
