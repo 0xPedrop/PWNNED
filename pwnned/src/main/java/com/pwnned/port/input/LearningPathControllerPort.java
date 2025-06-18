b5023ed193e3cc79c214487cdabbfd18fee045fa
0xJotave <joaovmb15@gmail.com>
2025-06-18 12:58:11 -0300

    Create Input Ports (Service and Controller) for Learning Path

---
+++ pwnned/src/main/java/com/pwnned/port/input/LearningPathControllerPort.java
@@ -0,0 +1,22 @@
+package com.pwnned.port.input;
+
+import com.pwnned.adapter.input.dto.LaboratoryDTO;
+import com.pwnned.adapter.input.dto.LearningPathDTO;
+import com.pwnned.domain.enums.Difficulty;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface LearningPathControllerPort {
+    ResponseEntity<LearningPathDTO> createLearningPath(@RequestBody LearningPathDTO learningPathDTO);
+    ResponseEntity<List<LearningPathDTO>> getAllLearningPaths();
+    ResponseEntity<LearningPathDTO> getSingleLearningPath(@PathVariable UUID learningPathId);
+    ResponseEntity<String> deleteLearningPath(@PathVariable UUID learningPathId);
+    ResponseEntity<String> deleteAllLearningPaths();
+    ResponseEntity<List<LearningPathDTO>> getLearningPathsByDifficulty(@PathVariable Difficulty difficulty);
+    ResponseEntity<List<LaboratoryDTO>> getLaboratoriesForLearningPath(
+            @PathVariable UUID learningPathId);
+}
