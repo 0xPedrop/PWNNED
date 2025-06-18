e07822835e8b80ca998eb8317c622f7b0760ba38
0xJotave <joaovmb15@gmail.com>
2025-06-18 12:59:06 -0300

    Create Output Port (Repository) for Laboratory

---
+++ pwnned/src/main/java/com/pwnned/port/output/LaboratoryRepositoryPort.java
@@ -0,0 +1,18 @@
+package com.pwnned.port.output;
+
+import com.pwnned.domain.enums.LaboratoryType;
+import com.pwnned.domain.model.Laboratory;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface LaboratoryRepositoryPort {
+    Laboratory save(Laboratory laboratory);
+    List<Laboratory> findAll();
+    Optional<Laboratory> findById(UUID laboratoryId);
+    List<Laboratory> getLaboratoriesByType(LaboratoryType laboratoryType);
+    void deleteById(UUID laboratoryId);
+    void deleteAll();
+    List<Laboratory> findByLearningPathId(UUID learningPathId);
+}
