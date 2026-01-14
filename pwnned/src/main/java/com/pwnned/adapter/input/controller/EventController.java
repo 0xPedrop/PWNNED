eb82bff3a33d04452e63847539352afd16a8975f
0xJotave <joaovmb15@gmail.com>
2026-01-13 22:58:48 -0300

    Creating Controller for Event

---
+++ pwnned/src/main/java/com/pwnned/adapter/input/controller/EventController.java
@@ -0,0 +1,52 @@
+package com.pwnned.adapter.input.controller;
+
+import com.pwnned.adapter.input.dto.EventDTO;
+import com.pwnned.port.input.EventControllerPort;
+import com.pwnned.port.input.EventServicePort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/events")
+public class EventController implements EventControllerPort {
+
+    private final EventServicePort eventServicePort;
+
+    public EventController(EventServicePort eventServicePort) {
+        this.eventServicePort = eventServicePort;
+    }
+
+    @Override
+    @PostMapping
+    public ResponseEntity<EventDTO> create(@RequestBody EventDTO dto) {
+        EventDTO created = eventServicePort.createEvent(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<List<EventDTO>> listAll() {
+        return ResponseEntity.ok(eventServicePort.getAllEvents());
+    }
+
+    @Override
+    @GetMapping("/nearby")
+    public ResponseEntity<List<EventDTO>> getNearby(
+            @RequestParam double lat,
+            @RequestParam double lon,
+            @RequestParam(defaultValue = "100") double radius) {
+
+        List<EventDTO> nearbyEvents = eventServicePort.getNearbyEvents(lat, lon, radius);
+        return ResponseEntity.ok(nearbyEvents);
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<EventDTO> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(eventServicePort.getEventById(id));
+    }
+}
